1b7650762072b791144bf88530895b3cca22d30f
jarvis-1805 <shubhsahu1805@gmail.com>
2022-07-10 23:43:49 +0530

    added navbar

--- includes/header.php
+++ includes/header.php
@@ -13,3 +13,15 @@
   </head>
   <body>
     <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="index.php">CTF-1.0</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="index.php">Home</a>
+          <a class="nav-item nav-link" href="viewrecords.php">View Attendees</a>
+        </div>
+      </div>
+    </nav>
