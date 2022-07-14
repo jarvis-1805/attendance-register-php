16203eccefe22b6a3b5c111b638458e47fa245e7
jarvis-1805 <shubhsahu1805@gmail.com>
2022-07-14 18:21:18 +0530

    Added edit confirmation function

--- edit.php
+++ edit.php
@@ -50,7 +50,30 @@
           <input type="text" class="form-control" value="<?php echo $attendee['contactnumber'] ?>" id="phone" aria-describedby="phoneHelp" name="phone">
           <small id="phoneHelp" class="form-text text-muted">We'll never share your phone number with anyone else.</small>
         </div>
-        <button type="submit" name="submit" class="btn btn-success btn-block">Save Changes</button>
+        <button type="button" name="submit" class="btn btn-secondary" onclick="location.href='viewrecords.php'">Back to List</button>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Save Changes</button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Save Changes</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                Are you sure to save the changes?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                <button type="submit" name="submit" class="btn btn-primary">Yes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
 
     <?php } ?>
